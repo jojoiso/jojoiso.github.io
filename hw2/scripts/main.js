d4923fd50d4d75d43d259133de3c87900c56ea2f
jojoiso <32134731+jojoiso@users.noreply.github.com>
2018-04-23 18:19:00 +0900

    Update main.js

--- hw2/scripts/main.js
+++ hw2/scripts/main.js
@@ -1,97 +1,102 @@
-d3.json("countries_2012.json", function(error, data) { //Не забыть потом вставить ссылку на JSON загруженного на гит
-console.log(data[0]['name'])
+var url="https://birulik.github.io/ITMO/hw2/countries_2012.json"
+d3.json(url, function(error, data){
+  //console.log(data[0]['name'])
+    
+	var isAscending = true;
+  var columns = Object.keys(data[0]);
+  //columns=columns.splice(1, 2, 4, 7, 8);
+	var colnames = ["name", "continent", "gdp", "life_expectancy", "population", "year"]
+  var table = d3.select("body").append("table"),
+    thead = table.append("thead")
+                 .attr("class", "thead");
+    tbody = table.append("tbody");
 
-    var columns = Object.keys(data[0]);
-    var colnames = ["name", "gdp", "life_expectancy", "year"];
-    var isAscending = true;
-    var table = d3.select("body").append("table"),
-        thead = table.append("thead")
-                     .attr("class", "thead");
-        tbody = table.append("tbody");
+  table.append("caption")
+    .html("World Countries Ranking");
 
-        table.append("caption")
-            .html("World Countries Ranking");
+  thead.append("tr").selectAll("th")
+    .data(colnames)  //new header
+  .enter()
+    .append("th")
+    .text(function(d) { return d; })
+    .on("click", function(header, i) {
+      d3.selectAll('th').attr("class","")
+      console.log(1)
+      if (isAscending) {
+    	 tbody.selectAll("tr").sort(function(a, b) {
+        return d3.descending(a[header], b[header]);
+      });
+        isAscending = false;
+        this.className = 'aes'
+      } else {
+        tbody.selectAll("tr").sort(function(a, b) {
+        return d3.ascending(a[header], b[header]);
+      });
+      	isAscending = true;
+        this.className = "des"
+      }
+    });
 
-        thead.append("tr").SelectorAll("th")
-            .data(colnames)
-            .enter()
-            .append("th")
-            .text(function(d) { return d; })
-            .on("click", function(header, i) {
-                d3.selectAll("th").attr("class", "")
-                console.log(1)
-                if (isAscending) {
-                    tbody.selectAll("tr").sort(function (a, b) {
-                        return d3.descending(a[header], b[header]);
-                    });
-                    isAscending = false;
-                    this.className = "aes"
-                } else {
-                    tbody.selectAll("tr").sort(function(a,b) {
-                        return d3.ascending(a[header], b[header]);
-                    });
-                    isAscending = true;
-                    this.className = "des"
-                }
-            });
-    console.log(data[0]['name'])
+  var rows = tbody.selectAll("tr.row")
+    .data(data)
+    .enter()
+    .append("tr").attr("class", "row");
 
-    var rows = tbody.selectAll("tr.row")
-        .data(data)
-        .enter()
-        .append("tr").attr("class", "row");
+  var cells = rows.selectAll("td")
+    .data(function(row) {   
+      
 
-    var cells = rows.selectAll("td")
-        .data(function(row) {
+      var newrow = []
+      for (i=0;i<colnames.length;i++) {
+        newrow.push(row[colnames[i]])
+      }
+      
 
-        var newrow = []
-        for (i=0; i<colnames.length; i++) {
-            newrow.push(row[colnames[i]])
-        }
-
-        return newrow
+     // console.log(newrow)
+      return newrow
+        /*return d3.range(Object.keys(row).length).map(function(column, i) {
+            return row[Object.keys(row)[i]];
+        });*/
     })
     .enter()
-        .append("td")
-        .text(function(d, i) {
-            switch(i) {
-                case 2:
-                        return d3.format(".2s")(d); //gdp
-                        break;
-                case 30:
-                        return d3.format(".1f")(d); //lt
-                        break;
-                case 3:
-                        return d3.format(".1f")(d); // life
-                        break;
-                case 50:
-                        return d3.format(".1f")(d); //long
-                        break;
-                case 4:
-                        return d3.format(",")(d); //pop
-                        break;
-                default:
-                        return d;
-            }
-        })
+    .append("td")
+    .text(function(d, i) {
+    	switch(i) {
+    		case 2:
+    			return d3.format(".2s")(d);//gdp
+    			break;
+    		case 30:
+    			return d3.format(".1f")(d);//lt
+    			break;
+    		case 3:
+    			return d3.format(".1f")(d);//life
+    			break;
+    		case 50:
+    			return d3.format(".1f")(d);//long
+    			break;
+    		case 4:
+    			return d3.format(",")(d);//pop
+    			break;
+    		default:
+    			return d;
+    	}
+     })
 
-    d3.selectAll("tr.row").each(function(row){
-        this.className += ""+row["continent"]
-    })
+d3.selectAll('tr.row').each(function(row){
+  this.className += ' '+row['continent']
+})
+d3.selectAll(".f1").each(function(d) { 
+  console.log(d3.select(this).attr("name"))
+})
 
-    d3.selectAll(".f1").each(function(d) {
-        console.log(d3.select(this).attr("name"))
-    })
-
-    d3.selectAll(".f1").on("change", function(e) {
-        console.log(this.checked, this.name)
-        if (this.checked) {
-            d3.selectAll("."+this.name).attr("class", "row "+this.name)
-        }
-        else {
-            d3.selectAll("."+this.name).attr("class", "row "+this.name+" hide")
-        }
-    })
-
+d3.selectAll(".f1").on("change", function(e){
+  console.log(this.checked, this.name)
+  if (this.checked){
+    d3.selectAll("."+this.name).attr("class", "row "+this.name)
+  }
+  else {
+    d3.selectAll("."+this.name).attr("class", "row "+this.name+" hide")
+  }
+})
 
 });
